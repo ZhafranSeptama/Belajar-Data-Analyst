50959f34b059822bf96bec9d2612a2e7138ff711
japran <zhafranseptama04@gmail.com>
2025-03-01 15:54:04 +0700

    Add files via upload

---
+++ intermediate MySQL/Case Statement.sql
@@ -0,0 +1,22 @@
+-- Case statement 
+# berguna untuk logical operation (sama kaya if else statement)
+SELECT first_name, 
+last_name, 
+CASE
+	WHEN age <= 30 THEN 'Young'
+    WHEN age BETWEEN 31 AND 50 THEN 'Old'
+    WHEN age >= 50 THEN "On Death's Door"
+END as age_bracket
+FROM employee_demographics;
+
+-- advance case statement 
+
+SELECT first_name, last_name, salary,
+CASE
+	WHEN salary > 45000 THEN salary + (salary * 0.05)
+    WHEN salary < 45000 THEN salary + (salary * 0.07)
+END AS new_salary,
+CASE
+	WHEN dept_id = 6 THEN salary * .10
+END AS Bonus
+FROM employee_salary;
